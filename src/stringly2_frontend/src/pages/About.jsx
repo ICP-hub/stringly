265a702fc6cba8dc23eb2abf3e177f0d67c45b76
harshith-sys <harshith.shetty@quadbtech.com>
2024-10-22 11:56:30 +0530

    code updating

--- src/stringly2_frontend/src/pages/About.jsx
+++ src/stringly2_frontend/src/pages/About.jsx
@@ -17,13 +17,12 @@ function About() {
 			<section>
 				<div className='relative w-full bg-black'>
 					<img src="./aboutHeroImg.png" alt="Premium Features" className='w-full h-screen object-cover sm:h-screen lg:h-auto opacity-[0.7]' />
-					<div className='w-[364px] flex items-center flex-col justify-center absolute top-60 md:top-80 text-center left-4 md:left-48 lg:top-[35%] lg:top-[35%] lg:left-[30%] lg:p-4 text-white lg:w-[560px] lg:h-[254px]'>
-						<h1 className='font-bold font-sf leading-[72px] lg:leading-[67px] text-[38px] lg:text-[56px]'>
+					<div className="absolute text-white left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[364px] p-2 md:w-[600px] bg-opacity-50 text-center">						<h1 className='font-bold font-sf leading-[72px] lg:leading-[67px] text-[38px] lg:text-[56px]'>
 							Discover Our Story.</h1>
 						<p className='text-[18px] text-center font-roboto font-regular leading-[27px]'>
 							Explore the vision behind Stringly and our commitment to redefining connections in a luxurious way.
 						</p>
-						<div class="flex space-x-4 my-10">
+						<div class="flex space-x-4 my-10 justify-center items-center">
 							<button class="px-10 py-4 bg-white text-black font-sf text-[16px] rounded-xl hover:bg-gray-100">
 								Join Us
 							</button>
@@ -34,14 +33,14 @@ function About() {
 					</div>
 				</div>
 			</section>
-			<section>
+			<section className='w-auto'>
 				{/* first card */}
 				<div
 					className={`ppp animate p-4 my-6 mt-16 py-8 lg:p-0 lg:flex relative items-center justify-between pt-24 ${hovered[0] ? 'hovered' : ''}`}
 					onMouseEnter={() => handleHover(0)}
 				>
-					<div className={`child hidden lg:block w-[616px] ml-[100px] m-auto opacity-0`}>
-						<p className='font-sf font-bold text-[40px] leading-[48px] py-8'>
+					<div className={`child hidden h-full lg:block w-[616px] ml-[100px] m-auto opacity-0`}>
+						<p className='font-sf font-bold text-[40px] leading-[48px] py-8 w-screen lg:w-auto mt-20 lg:mt-0'>
 							Our Journey: Crafting Meaningful Connections Through Luxury
 						</p>
 						<p className='text-[18px] font-jakarta font-regular leading-[27px]'>
@@ -66,8 +65,8 @@ function About() {
 							</div>
 						</div>
 					</div>
-					<div className='lg:w-auto z-20 left-right children bg-black'>
-						<img src="./aboutPageImage1.png" alt="" className='h-screen lg:h-auto object-cover opacity-[0.7]' />
+					<div className='lg:w-auto h-[657px] lg:h-auto z-20 left-right children bg-black'>
+						<img src="./aboutPageImage1.png" alt="PageImage1" className='h-screen lg:h-auto object-cover opacity-[0.7]' />
 					</div>
 				</div>
 				{/* second card */}
@@ -113,37 +112,32 @@ function About() {
 					<h1 className='text-[48px] font-bold font-roboto text-black mt-16 mb-4'>Our Team</h1>
 					<p className='text-gray-600 text-[18px] font-bold font-roboto mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 				</div>
-				<div class="p-4 md:p-2 flex items-center justify-center mb-16 animate">
-					<div class="mx-auto grid grid-cols-2 lg:grid-cols-3 gap-4 animate lg:gap-8 lg:w-[1285px]">
-						<div class="flex w-[174px] h-[231px] md:w-[300px] md:h-[360px] lg:w-[407px] lg:h-[540px] flex-col p-0">
-							<img src="./aboutDefault1.png" alt="Erika Fisher" class="w-full h-full object-cover" />
+				<div className="p-4 md:p-2 flex items-center justify-center mb-16 animate">
+					<div className="mx-auto grid grid-cols-2 lg:grid-cols-3 gap-4 animate lg:gap-8 lg:w-[1285px]">
+						<div className="flex w-[174px] h-[231px] md:w-[300px] md:h-[360px] lg:w-[407px] lg:h-[540px] flex-col p-0">
+							<img src="./aboutDefault1.png" alt="Erika Fisher" className="w-full h-full object-cover" />
 						</div>
-						<div class="flex w-[174px] h-[231px] md:w-[300px] md:h-[360px] lg:w-[407px] lg:h-[540px] flex-col p-0">
-							<img src="./aboutDefault2.png" alt="Erika Fisher" class="w-full h-full object-cover" />
+						<div className="flex w-[174px] h-[231px] md:w-[300px] md:h-[360px] lg:w-[407px] lg:h-[540px] flex-col p-0">
+							<img src="./aboutDefault2.png" alt="Erika Fisher" className="w-full h-full object-cover" />
 						</div>
-						<div class="flex w-[174px] h-[231px] md:w-[300px] md:h-[360px] lg:w-[407px] lg:h-[540px] flex-col p-0">
-							<img src="./aboutDefault3.png" alt="Erika Fisher" class="w-full h-full object-cover" />
+						<div className="flex w-[174px] h-[231px] md:w-[300px] md:h-[360px] lg:w-[407px] lg:h-[540px] flex-col p-0">
+							<img src="./aboutDefault3.png" alt="Erika Fisher" className="w-full h-full object-cover" />
 						</div>
 
-						<div class="flex w-[174px] h-[231px] md:w-[300px] md:h-[360px] lg:w-[407px] lg:h-[540px] flex-col p-0">
-							<img src="./aboutDefault4.png" alt="Erika Fisher" class="w-full h-full object-cover" />
+						<div className="flex w-[174px] h-[231px] md:w-[300px] md:h-[360px] lg:w-[407px] lg:h-[540px] flex-col p-0">
+							<img src="./aboutDefault4.png" alt="Erika Fisher" className="w-full h-full object-cover" />
 						</div>
-						<div class="flex w-[174px] h-[231px] md:w-[300px] md:h-[360px] lg:w-[407px] lg:h-[540px] flex-col p-0">
-							<img src="./aboutDefault5.png" alt="Erika Fisher" class="w-full h-full object-cover" />
+						<div className="flex w-[174px] h-[231px] md:w-[300px] md:h-[360px] lg:w-[407px] lg:h-[540px] flex-col p-0">
+							<img src="./aboutDefault5.png" alt="Erika Fisher" className="w-full h-full object-cover" />
 						</div>
-						<div class="flex w-[174px] h-[231px] md:w-[300px] md:h-[360px] lg:w-[407px] lg:h-[540px] flex-col p-0">
-							<img src="./aboutDefault6.png" alt="Erika Fisher" class="w-full h-full object-cover" />
+						<div className="flex w-[174px] h-[231px] md:w-[300px] md:h-[360px] lg:w-[407px] lg:h-[540px] flex-col p-0">
+							<img src="./aboutDefault6.png" alt="Erika Fisher" className="w-full h-full object-cover" />
 						</div>
 					</div>
 				</div>
 			</main>
-			{/* <section>
-				<div className='relative w-full'>
-					<img src="./about/Frame42.png" alt="Premium Features" className='w-full h-screen object-cover md:h-auto' />
-				</div>
-			</section> */}
 			<section>
-				<div className='relative w-full h-[656px] md:h-auto bg-black mx-4'>
+				<div className='relative w-full h-[656px] md:h-auto bg-black'>
 					<img src="./aboutComponent 2.png" alt="Premium Features" className='w-full lg:w-screen relative object-cover h-full md:h-screen opacity-[0.7]' />
 					<div className='hidden lg:flex items-center gap-8 flex-col justify-center absolute top-40 lg:top-80 text-center left-4 md:top-[35%] lg:top-[35%] lg:left-[30%] p-4 text-white lg:w-[560px] lg:h-[254px]'>
 						<h1 className='font-semibold lg:font-bold font-sf leading-[38px] md:leading-[67px] text-[32px] md:text-[56px]'>
@@ -151,13 +145,13 @@ function About() {
 						<p className='text-[14px] md:text-[18px] text-center font-jakarta font-regular leading-[21px] lg:leading-[27px]'>
 							At Stringly, we utilize the Internet Computer Protocol (ICP) to provide a secure and private environment for our users. This cutting-edge technology ensures that your personal data remains confidential while you explore meaningful connections. With Stringly, you can enjoy peace of mind knowing that your privacy is our top priority.
 						</p>
-						<div class="flex space-x-4 my-10">
-							<button class="px-8 py-4 border-2 border-white text-white text-lg rounded-xl hover:bg-black hover:text-white transition-colors">
+						<div className="flex space-x-4 my-10">
+							<button className="px-8 py-4 border-2 border-white text-white text-lg rounded-xl hover:bg-black hover:text-white transition-colors">
 								Learn More
 							</button>
-							<button class="px-8 py-4 text-white text-xl flex items-center space-x-2 hover:underline">
+							<button className="px-8 py-4 text-white text-xl flex items-center space-x-2 hover:underline">
 								<span>Sign Up</span>
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5">
 									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M12 5l7 7-7 7" />
 								</svg>
 							</button>
@@ -166,7 +160,7 @@ function About() {
 				</div>
 			</section>
 			<div className='hidden lg:block'>
-			<TestimonialGrid />
+				<TestimonialGrid />
 			</div>
 			<section>
 				<div className="h-full py-12 mb-12 px-6 flex flex-col box-contain">
